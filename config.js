868bfb388df4c71efc20f35cd4d60d17c1f739d8
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-03-11 15:22:02 +0700

    Anjayy

---
+++ config.js
@@ -0,0 +1,23 @@
+global.owner = ['6282252655313']
+global.mods = ['6282252655313']
+global.prems = ['6282252655313']
+global.APIs = { 
+  nrtm: 'https://nurutomo.herokuapp.com',
+  xteam: 'https://api.xteam.xyz'
+}
+global.APIKeys = { 
+  'https://api.xteam.xyz': 'd90a9e986e18778b'
+}
+
+
+
+
+let fs = require('fs')
+let chalk = require('chalk')
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+  fs.unwatchFile(file)
+  console.log(chalk.redBright("Update 'config.js'"))
+  delete require.cache[file]
+  require(file)
+})
