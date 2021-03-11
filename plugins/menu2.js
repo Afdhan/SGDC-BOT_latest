ed685eac4f965dc062e782b3e6ff0fd709385e3c
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-03-11 20:11:38 +0700

    Add files via upload

--- plugins/menu2.js
+++ plugins/menu2.js
@@ -1,59 +1,22 @@
-let fs = require ('fs')
-let path = require('path')
-let { performance } = require('perf_hooks')
-let old = performance.now()
-let neww = performance.now()
-
-let ping = performance.now(neww - old)
-    let date = d.toLocaleDateString(locale, {
-      day: 'numeric',
-      month: 'long',
-      year: 'numeric'
-    })
-    let time = d.toLocaleTimeString(locale, {
-      hour: 'numeric',
-      minute: 'numeric',
-      second: 'numeric'
-    })
-    let _uptime = process.uptime() * 1000
-    let _muptime
-    if (process.send) {
-      process.send('uptime')
-      _muptime = await new Promise(resolve => {
-        process.once('message', resolve)
-        setTimeout(resolve, 1000)
-      }) * 1000
-    }
-    let muptime = clockString(_muptime)
-    let uptime = clockString(_uptime)
-let week = d.toLocaleDateString(locale, { weekday: 'long' })
-let name = conn.getName(m.sender)
-let handler  = async (m, { conn, usedPrefix  }) => {`
+let handler  = async (m, { conn, usedPrefix }) => {`
 *┏━━━━━━━━━━━━━━━━━━━━┓*
 *┃══════᳀ SGDC - BOT ᳀══════*
 *┣━━━━━━━━━━━━━━━━━━━━┛*
 *┃╭══════════════════╮*
-*┃║ YourName : _${name}_*
 *┃║ BotName :    SGDC - BOT*
 *┃║ Version :       1.4.0 (beta)*
-*┃║ Prefix :          ( ${_p} )*
-*┃║ Date :             ${week}, ${date}*
-*┃║ Time :            ${time}*
-*┃║ Uptime :        _${uptime} (${muptime})_*
-*┃║ PingSpeed : _${ping} MS_*
 *┃╰══════════════════╯*
 *┣━━━━━━━━━━━━━━━━━━━━┓*
-*┃══════᳀ LIST MENU ᳀══════*
+*┃══════᳀ TOOLS MENU ᳀═════*
 *┣━━━━━━━━━━━━━━━━━━━━┛*
 *┃╭══════════════════╮*
-*┃║★ ${usedPrefix}gretongmenu*
-*┃║★ ${usedPrefix}makermenu*
-*┃║★ ${usedPrefix}internetmenu*
-*┃║★ ${usedPrefix}groupmenu*
-*┃║★ ${usedPrefix}adminmenu*
-*┃║★ ${usedPrefix}ownermenu*
-*┃║★ ${usedPrefix}optionmenu*
-*┃║★ ${usedPrefix}toolsmenu*
+*┃║★ ${usedPrefix}ping*
+*┃║★ ${usedPrefix}owner*
+*┃║★ ${usedPrefix}donasi*
+*┃║★ ${usedPrefix}grouplist*
+*┃║★ ${usedPrefix}afk*  [Reason]
+*┃║★ ${usedPrefix}base64*  [Text]
+*┃║★ ${usedPrefix}profil* [@user]
 *┃╰══════════════════╯*
 *┣━━━━━━━━━━━━━━━━━━━━┓*
 *┃═══════᳀  INGFO  ᳀═══════*
@@ -71,7 +34,7 @@ let handler  = async (m, { conn, usedPrefix  }) => {`
 *┗━━━━━━━━━━━━━━━━━━━━┛*
 `.trim()) 
 
-handler.command = /^(nenen)$/i
+handler.command = /^(tools(menu))$/i
 handler.owner = false
 handler.mods = false
 handler.premium = false
