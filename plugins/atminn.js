68027518178ac0752ddcce05611d302a4ce1108a
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-03-11 15:34:32 +0700

    Anjayy

---
+++ plugins/atminn.js
@@ -0,0 +1,47 @@
+let handler  = async (m, { conn, usedPrefix: _p }) => {`
+*┏━━━━━━━━━━━━━━━━━━━━┓*
+*┃══════᳀ SGDC - BOT ᳀══════*
+*┣━━━━━━━━━━━━━━━━━━━━┛*
+*┃╭══════════════════╮*
+*┃║ BotName :    SGDC - BOT*
+*┃║ Version :       1.4.0 (beta)*
+*┃╰══════════════════╯*
+*┣━━━━━━━━━━━━━━━━━━━━┓*
+*┃══════᳀ ADMIN MENU ᳀═════*
+*┣━━━━━━━━━━━━━━━━━━━━┛*
+*┃╭══════════════════╮*
+*┃║★ ${_p}add*  [628xx,628xx]
+*┃║★ ${_p}demote*  [@user]
+*┃║★ ${_p}promote*  [@user]
+*┃║★ ${_p}kick*  [@user]
+*┃╰══════════════════╯*
+*┣━━━━━━━━━━━━━━━━━━━━┓*
+*┃═══════᳀  INGFO  ᳀═══════*
+*┣━━━━━━━━━━━━━━━━━━━━┛*
+*┃╭══════════════════╮*
+*┃║ Grup: t.me/SGDC_TEAM*
+*┃║ Grup: t.me/DesaConfig*
+*┃║ Channel: t.me/SobatGretong*
+*┃║ Channel: t.me/DesaConfigCh*
+*┃║ Donate: nyawer.co/SGDC*
+*┃║ Donate: saweria.co/AFD11*
+*┃╰══════════════════╯*
+*┣━━━━━━━━━━━━━━━━━━━━┓*
+*┃══════᳀ SGDC - BOT ᳀══════*
+*┗━━━━━━━━━━━━━━━━━━━━┛*
+`.trim()) 
+
+handler.command = /^(admin?menu)$/i
+handler.owner = false
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = false
+
+handler.admin = false
+handler.botAdmin = false
+
+handler.fail = null
+
+
+module.exports = handler
