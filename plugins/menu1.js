af36dafc0f77f5382914ce2ac1d10970d1c59b2e
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-03-11 20:30:13 +0700

    Update menu1.js

--- plugins/menu1.js
+++ plugins/menu1.js
@@ -73,7 +73,6 @@ let handler  = async (m, { conn, usedPrefix: _p }) => {
           if (menu.help) groups[tag].push(menu)
     }
     conn.menu = { `
-//    let before = conn.menu.before || `
 *┏━━━━━━━━━━━━━━━━━━━━┓*
 *┃══════᳀ SGDC - BOT ᳀══════*
 *┣━━━━━━━━━━━━━━━━━━━━┛*
@@ -113,11 +112,10 @@ let handler  = async (m, { conn, usedPrefix: _p }) => {
 *┣━━━━━━━━━━━━━━━━━━━━┓*
 *┃══════᳀ SGDC - BOT ᳀══════*
 *┗━━━━━━━━━━━━━━━━━━━━┛*`
-/*%readmore`
-    let header = conn.menu.header || '╭─「 %category 」'
-    let body   = conn.menu.body   || '│ • %cmd%islimit'
-    let footer = conn.menu.footer || '╰────\n'
-    let after  = conn.menu.after  || (conn.user.jid == global.conn.user.jid ? '' : `Powered by https://wa.me/${global.conn.user.jid.split`@`[0]}`) + `\n*%npmname@^%version*\n\`\`\`\%npmdesc\`\`\``
+//    let header = conn.menu.header || '╭─「 %category 」'
+//    let body   = conn.menu.body   || '│ • %cmd%islimit'
+ //   let footer = conn.menu.footer || '╰────\n'
+ //   let after  = conn.menu.after  || (conn.user.jid == global.conn.user.jid ? '' : `Powered by https://wa.me/${global.conn.user.jid.split`@`[0]}`) + `\n*%npmname@^%version*\n\`\`\`\%npmdesc\`\`\``
 /*  let _text  = before + '\n'
     for (let tag in groups) {
       _text += header.replace(/%category/g, tags[tag]) + '\n'
