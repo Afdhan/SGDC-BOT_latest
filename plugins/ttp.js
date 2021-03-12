da0c75366f45e9fe97220d0f0a3b4d36f35b7323
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-03-12 20:43:11 +0700

    Update ttp.js

--- plugins/ttp.js
+++ plugins/ttp.js
@@ -16,7 +16,7 @@ handler.owner = false
 handler.mods = false
 handler.premium = false
 handler.group = false
-handler.private = true
+handler.private = false
 
 handler.admin = false
 handler.botAdmin = false
