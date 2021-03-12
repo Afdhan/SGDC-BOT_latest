ecf5fb2e7b91b3defb8b35f17492936fa51957bb
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-03-12 21:23:40 +0700

    Update banchat.js

--- plugins/banchat.js
+++ plugins/banchat.js
@@ -1,11 +1,10 @@
 let handler = async (m, { conn, participants }) => {
     if (participants.map(v=>v.jid).includes(global.conn.user.jid)) {
     global.DATABASE._data.chats[m.chat].isBanned = true
-    m.reply('Done..!')
+    m.reply('Satu Chat Grup Berhasil Dibanned!')
    } else m.reply('Ada nomor host disini...')
 }
-handler.help = ['banchat']
-handler.tags = ['owner']
+
 handler.command = /^(banchat|ban)$/i
 handler.owner = true
 
