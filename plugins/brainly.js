49087fe57bd361511b3ce579e83d6902270e73db
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-03-13 08:07:26 +0700

    Add files via upload

--- plugins/brainly.js
+++ plugins/brainly.js
@@ -1,13 +1,18 @@
 const brainly = require('brainly-scraper-v2')
 let handler = async function (m, { text }) {
-  if (!text) throw 'Soalnya?'
+  if (!text) throw 'Pertanyaannya mana?'
   let res = await brainly(text)
-  let answer = res.data.map((v, i) => `_*PERTANYAAN KE ${i + 1}*_\n${v.pertanyaan}\n${v.jawaban.map((v,i) => `*JAWABAN KE ${i + 1}*\n${v.text}`).join('\n')}`).join('\n\n•------------•\n\n')
+    let answer = res.data.map((v, i) => `*━━► ᴩᴇʀᴛᴀɴyᴀᴀɴ ᴋᴇ ${i + 1}:*\n${v.pertanyaan}\n\n${v.jawaban.map((v,i) => `*━━► ᴊᴀᴡᴀʙᴀɴ ᴋᴇ ${i + 1}*\n${v.text}`).join('\n')}`).join('\n\n\n*[ • SGDC-BOT • ]━━━━━━━━━━━━━►*\n\n\n')
   m.reply(answer)
 }
 handler.help = ['brainly <soal>']
 handler.tags = ['internet']
 
-handler.command = /^brainly$/i
+handler.command = /^(brain|brainly|bl)$/i
+handler.owner = false
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = true
 
 module.exports = handler
