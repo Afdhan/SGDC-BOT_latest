adf6b49211dbb901195f8b6bc85a8d34049b7c43
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-03-14 08:10:16 +0700

    Update addprems.js

--- plugins/addprems.js
+++ plugins/addprems.js
@@ -8,7 +8,7 @@ let who
   // if (participants.map(v=>v.jid).includes(global.conn.user.jid)) {
     global.DATABASE._data.chats[m.chat].premium = true
   var nomor = m.mentionedJid[0]
-    m.reply(`*_Berhasil Menambahkan User!_*\n\n*Nomor : wa.me/${nomor.split("@s.whatsapp.net")[0]}*`)
+    m.reply(`*_Berhasil Menambahkan User!_*\n*Nomor : https://wa.me/${nomor.split("@s.whatsapp.net")[0]}*`)
   // } else m.reply('Ada nomor host disini...')
 }
 
