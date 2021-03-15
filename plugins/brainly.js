90874c2c77154cd2b72d7715eaf0e17d34474e36
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-03-15 20:26:22 +0700

    Update brainly.js

--- plugins/brainly.js
+++ plugins/brainly.js
@@ -4,7 +4,7 @@ let handler = async function (m, { text, err }) {
   let res = await brainly(text)
     let answer = res.data.map((v, i) => `*━━► ᴩᴇʀᴛᴀɴyᴀᴀɴ ᴋᴇ ${i + 1}:*\n${v.pertanyaan}\n\n${v.jawaban.map((v,i) => `*━━► ᴊᴀᴡᴀʙᴀɴ ᴋᴇ ${i + 1}*\n${v.text}`).join('\n')}`).join('\n\n\n*[ • SGDC-BOT • ]━━━━━━━━━━━━━►*\n\n\n')
   m.reply(answer)
-  if (err)  m.reply('*Maaf _Brainly-Scraper_ Saat Ini Sedang Error!*')
+  if (err)  return reply('*Maaf _Brainly-Scraper_ Saat Ini Sedang Error!*')
 }
 
 handler.command = /^(brain|brainly|bl)$/i
