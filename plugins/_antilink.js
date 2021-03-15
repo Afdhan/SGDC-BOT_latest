98234b3341c658f4ae09b562b0458c181f225a62
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-03-15 11:32:52 +0700

    Kontol

--- plugins/_antilink.js
+++ plugins/_antilink.js
@@ -1,14 +1,16 @@
-let handler = m => m
+let handler = async (m, { conn, args }) => {
 
 let linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})/i
 handler.before = m => {
   if (m.isBaileys && m.fromMe) return true
   let chat = global.DATABASE.data.chats[m.chat]
-  let users = global.DATABASE._data.users[m.sender]
+  let user = global.DATABASE._data.users[m.sender]
   let isGroupLink = linkRegex.exec(m.text)
 
-  if (chat.antiLink && isGroupLink) m.reply('*_Link Group Terdeteksi!_*\n*Anda Akan Dikeluarkan Otomatis Oleh SGDC-BOT*')
-  await conn.groupRemove([users])
+  if (chat.antiLink && isGroupLink) 
+      m.reply('*Link Group Terdeteksi! Anda Akan Dikeluarkan Otomatis Oleh SGDC-BOT*').then(() => {
+      conn.groupRemove(m.chat, [user])
+})
   return true
 }
 
