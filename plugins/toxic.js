098856c4a2cdf896af6cc6016d96152850739aaa
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-03-16 19:20:28 +0700

    Update toxic.js

--- plugins/toxic.js
+++ plugins/toxic.js
@@ -1,8 +1,7 @@
 let Regex = require('url-regex')
-let handler = m => 
+let handler = m => m
 let bad = /(anjing|kontol|bangsat|ngentot|ngewe|memek|tolol|pepek|goblo|gblk|ngeue|kntl|jancok|ajg|asu|ngntd|bgsd|bgst)/i
-/*let users = m.sender.filter(u => !(u.includes(conn.user.jid)))
-  for (let user of users) await conn.groupRemove(m.chat, [user])*/
+
 handler.before = m => {
   if (m.isBaileys && m.fromMe) return true
   let chat = global.DATABASE.data.chats[m.chat]
@@ -14,13 +13,8 @@ handler.before = m => {
  _Tolong Gausah Sok Toxic Mastah!_
  *_Nanti Kalau Dikick Jangan Protes!!_*
 `.trim())
-  return true
-     
+  return true    
 }
-
-/*function pickRandom(list) {
-  return list[Math.floor(list.length * Math.random())]
-}*/
 
 
 module.exports = handler
