e4c3882f03d85fdbd85a6dfb9c83dc647b1ad4a8
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-03-16 12:25:48 +0700

    Update toxic.js

--- plugins/toxic.js
+++ plugins/toxic.js
@@ -7,7 +7,7 @@ handler.before = m => {
   if (m.isBaileys && m.fromMe) return true
   let chat = global.DATABASE.data.chats[m.chat]
   let isBadWord = bad.exec(m.text)
-  if (chat.antiToxic && isBadWord) m.reply('Kata Toxic Terdeteksi! Anda Akan Dikeluarkan Otomatis Oleh SGDC-BOT')
+  if (chat.badWord && isBadWord) m.reply('Kata Toxic Terdeteksi! Anda Akan Dikeluarkan Otomatis Oleh SGDC-BOT')
   return true
 }
 
