7fc6ac36a059b98eabebb26cdc5a89bde22e25eb
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-03-16 12:53:49 +0700

    Update toxic.js

--- plugins/toxic.js
+++ plugins/toxic.js
@@ -1,15 +1,18 @@
+let path = require('path)
 let fs = require('fs')
 let handler = m => m
-let bad = fs.readdirSync(global.bad)
+let bad = fs.readdirSync(bad)
+let AFDHN = Link.slice(0).trim().split(/ +/).shift().toLowerCase()
 handler.before = m => {
   if (m.isBaileys && m.fromMe) return true
+  if (bad.includes(AFDHN))
   let chat = global.DATABASE.data.chats[m.chat]
   let isBadWord = bad.exec(m.text)
   if (chat.badWord && isBadWord) m.reply('*_No Toxic_*')
   return true
 }
 
-global.bad = [
+bad = [
 'anjing','kontol','bangsat',
 'ngentot','ngewe','memek','tolol',
 ]
