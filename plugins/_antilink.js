a1617d1a09560f69179be7b23f32490e68a81d18
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-03-16 07:20:10 +0700

    Add files via upload

--- plugins/_antilink.js
+++ plugins/_antilink.js
@@ -1,16 +1,11 @@
-let handler = m => 
+let handler = m => m
 
 let linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})/i
 handler.before = m => {
   if (m.isBaileys && m.fromMe) return true
   let chat = global.DATABASE.data.chats[m.chat]
-  //let user = global.DATABASE._data.users[m.sender]
   let isGroupLink = linkRegex.exec(m.text)
-
-  if (chat.antiLink && isGroupLink) 
-      m.reply('*Link Group Terdeteksi! Anda Akan Dikeluarkan Otomatis Oleh SGDC-BOT*')/*.then(() => {
-      conn.groupRemove(m.chat, [user])
-})*/
+  if (chat.antiLink && isGroupLink) m.reply('*Link Group Terdeteksi!*\n*_Hapus Atau Kick?!?!_*')
   return true
 }
 
