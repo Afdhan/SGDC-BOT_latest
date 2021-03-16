f662c09087af637f7cdc73c532a06bfc8f4e3dff
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-03-16 10:24:41 +0700

    Add files via upload

--- plugins/toxic.js
+++ plugins/toxic.js
@@ -1,17 +1,20 @@
+let path = require('path')
+let fs = require('fs')
 let handler = m => m
 
-let toxic = pickRandom(['anjing','kontol','bangsat','ngentot','ngewe','memek','tolol'])
+let bad = fs.readdirSync(global.bad)
 handler.before = m => {
   if (m.isBaileys && m.fromMe) return true
   let chat = global.DATABASE.data.chats[m.chat]
-  
-
-  if (chat.antiToxic) m.reply('Kata Toxic Terdeteksi! Anda Akan Dikeluarkan Otomatis Oleh SGDC-BOT')
-  conn.groupRemove([m.chat])
+  let isBadWord = bad.exec(m.text)
+  if (chat.antiToxic && isBadWord) m.reply('Kata Toxic Terdeteksi! Anda Akan Dikeluarkan Otomatis Oleh SGDC-BOT')
   return true
 }
 
-
+global.bad = [
+'anjing','kontol','bangsat',
+'ngentot','ngewe','memek','tolol',
+]
 
 handler.botAdmin = true
 
