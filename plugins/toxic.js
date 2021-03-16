326f27a24e3b4e62aa77974d052431b1d6a75442
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-03-16 18:05:33 +0700

    Update toxic.js

--- plugins/toxic.js
+++ plugins/toxic.js
@@ -1,10 +1,10 @@
 let Regex = require('url-regex')
 let handler = m => m
-let badRegex = ['anjing','kontol','bangsat','ngentot','ngewe','memek','tolol','pepek','goblok','gblk','ngeue','kntl','jancok','ajg','asu','ngntd','bgsd','bgst']
+let bad = /(anjing|kontol|bangsat|ngentot|ngewe|memek|tolol|pepek|goblo|gblk|ngeue|kntl|jancok|ajg|asu|ngntd|bgsd|bgst)/i
 handler.before = m => {
   if (m.isBaileys && m.fromMe) return true
   let chat = global.DATABASE.data.chats[m.chat]
-  let isBadWord = badRegex.exec(m.text)
+  let isBadWord = bad.exec(m.text)
   if (chat.badWord && isBadWord) m.reply('Ngontoll')
   return true
 }
