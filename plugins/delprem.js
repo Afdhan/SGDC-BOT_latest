e8f6a5fd8eda3e1a41a43969d04779f66d9d5f75
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-03-17 11:38:19 +0700

    Update delprem.js

--- plugins/delprem.js
+++ plugins/delprem.js
@@ -5,11 +5,11 @@ let who
   if (m.isGroup) who = m.mentionedJid[0]
   else who = m.chat
   if (!who) throw 'Tag salah satu lah,dan masukkan nomor untuk di verivikasi !'
-  // if (participants.map(v=>v.jid).includes(global.conn.user.jid)) {
+  if (participants.map(v=>v.jid).includes(global.conn.user.jid)) {
     global.DATABASE._data.chats[m.chat].premium = false
   var nomor = m.mentionedJid[0]
     m.reply(`*_Berhasil Menghapus User!_*\n*Nomor : https://wa.me/${nomor.split("@s.whatsapp.net")[0]}*`)
-  // } else m.reply('Ada nomor host disini...')
+   } else m.reply('Ada nomor host disini...')
 }
 
 handler.command = /^delprem$/i
