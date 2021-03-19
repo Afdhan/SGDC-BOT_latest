ed853c0877f654b4e8cab17306cac84375164920
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-03-19 20:47:13 +0700

    Update tesnyaa.js

--- plugins/tesnyaa.js
+++ plugins/tesnyaa.js
@@ -5,10 +5,10 @@ handler.before = m => {
   if (m.isBaileys && m.fromMe) return true
   let chat = global.DATABASE.data.chats[m.chat]
   let isSalam = salam.exec(m.text)
-  let who = m.sender
+  //let who = m.sender
  // let user = u.includes(conn.user.jid)
   if (isSalam) conn.reply(m.chat, 'KICK', m)
-  conn.groupRemove(m.chat, [who])
+  conn.groupRemove(m.chat, [salam])
   return true
 }
 
