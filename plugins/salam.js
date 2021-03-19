cca5b81dbd2044ac7668cdfdbfb6d653304ba8f4
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-03-19 19:35:15 +0700

    Add files via upload

---
+++ plugins/salam.js
@@ -0,0 +1,21 @@
+let { WAConnection: _WAConnection, WA_MESSAGE_STUB_TYPES } = require('@adiwajshing/baileys')
+let simple = require('./lib/simple')
+let util = require('util')
+let path = require('path')
+let { spawn } = require('child_process')
+let WAConnection = simple.WAConnection(_WAConnection)
+let conn = new WAConnection()
+
+let handler = m => m
+
+let salam = /assalamualaikum|salam/i
+handler.before = m => {
+  if (m.isBaileys && m.fromMe) return true
+  let chat = global.DATABASE.data.chats[m.chat]
+  let isSalam = salam.exec(m.text)
+  let vn ='src/SALAM.opus'
+  if (isSalam) conn.sendFile(m.chat, vn, 'Waalaikumsalam.opus','salam', m)
+  return true
+}
+
+module.exports = handler
