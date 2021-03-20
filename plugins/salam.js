81ab15f363a037dc0b62ae4383b39521f2134596
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-03-20 08:48:44 +0700

    Update salam.js

--- plugins/salam.js
+++ plugins/salam.js
@@ -1,14 +1,10 @@
-//let { WAConnection, WA_MESSAGE_STUB_TYPES } = require('@adiwajshing/baileys')
-//let simple = require('./lib/simple')
 let util = require('util')
 let path = require('path')
 let { spawn } = require('child_process')
-//let WAConnection = simple.WAConnection(_WAConnection)
-//let conn = new WAConnection()
 
 let handler = async (m, { conn, args }) => m
 
-let salam = /assalamualaikum|salam/i
+let salam = /^(assalamualaikum|asalamualaikum|assalamualikum|salamualaikum|salam)$/i
 handler.before = m => {
   if (m.isBaileys && m.fromMe) return true
   let chat = global.DATABASE.data.chats[m.chat]
