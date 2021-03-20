c00f3e0ab4f4a19289d5aafa631f00d77349c19a
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-03-20 13:22:52 +0700

    Add files via upload

---
+++ plugins/reprot.js
@@ -0,0 +1,24 @@
+const { MessageType } = require('@adiwajshing/baileys')
+
+let handler = async(m, { conn, text }) => {
+    if (!text) return conn.reply(m.chat, '_Masukkan Text Laporan!_', m)
+    if (text > 300) return conn.reply(m.chat, 'Teks Terlalu Panjang!, Maksimal 300 Karakter', m)
+    var nomor = m.sender
+    const teks1 = `*[ REPORT ]*\n*Nomor:* wa.me/${nomor.split("@s.whatsapp.net")[0]}\n*Laporan:*${text}`
+    conn.sendMessage('6282252655313@s.whatsapp.net', teks1, MessageType.text)
+    conn.reply(m.chat, '*[ SUKSES ] _Masalah telah di laporkan ke *Owner Bot!_*', m)
+}
+
+handler.command = /^(bug|report|bugreport)$/i
+handler.owner = false
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = false
+
+handler.admin = false
+handler.botAdmin = false
+
+handler.fail = null
+
+module.exports = handler
