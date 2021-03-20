c66b007a0dc0ea6458e48922ead2daf8874f044e
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-03-20 13:41:06 +0700

    Update reprot.js

--- plugins/reprot.js
+++ plugins/reprot.js
@@ -4,9 +4,9 @@ let handler = async(m, { conn, text }) => {
     if (!text) return conn.reply(m.chat, '_Masukkan Text Laporan!_', m)
     if (text > 300) return conn.reply(m.chat, 'Teks Terlalu Panjang!, Maksimal 300 Karakter', m)
     var nomor = m.sender
-    const teks1 = `*[ REPORT ]*\n*Nomor:* wa.me/${nomor.split("@s.whatsapp.net")[0]}\n*Laporan:*${text}`
+    const teks1 = `*[ REPORT ]*\n*Nomor:* https://wa.me/${nomor.split("@s.whatsapp.net")[0]}\n*Laporan:* ${text}\n\n*[ • SGDC-BOT •  ]*`
     conn.sendMessage('6282252655313@s.whatsapp.net', teks1, MessageType.text)
-    conn.reply(m.chat, '*[ SUKSES ] _Masalah telah di laporkan ke *Owner Bot!_*', m)
+    conn.reply(m.chat, '*[ SUKSES ]* _Masalah telah di laporkan ke *Owner Bot!_*', m)
 }
 
 handler.command = /^(bug|report|bugreport)$/i
