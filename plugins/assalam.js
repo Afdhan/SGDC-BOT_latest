e00ec2c698013bb5bcd68446d1cf462c652a4714
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-03-20 08:57:37 +0700

    Add files via upload

---
+++ plugins/assalam.js
@@ -0,0 +1,17 @@
+let util = require('util')
+let path = require('path')
+let { spawn } = require('child_process')
+
+let handler = async (m, { conn, args }) => m
+
+let salam = /^p(unten|ermisi)$/i
+handler.before = m => {
+  if (m.isBaileys && m.fromMe) return true
+  let chat = global.DATABASE.data.chats[m.chat]
+  let isSalam = salam.exec(m.text)
+  let vn ='src/ASSALAM.opus'
+  if (isSalam) conn.sendFile(m.chat, vn, 'Assalam.opus','salam', m)
+  return true
+}
+
+module.exports = handler
