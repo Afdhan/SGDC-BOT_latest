c6527a81e09048e5d5eddfcf66b28d0a9e69a756
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-03-21 19:18:52 +0700

    Add files via upload

--- plugins/toxic.js
+++ plugins/toxic.js
@@ -1,21 +1,21 @@
-let Regex = require('url-regex')
 let handler = m => m
-let bad = /(anjing|kontol|bangsat|ngentot|ngewe|memek|tolol|pepek|goblok|goblog|gblk|ngeue|kntl|jancok|ajg|ajglah|asu|ngntd|bgsd|kntod|kntd|bgst)/i
+
+let toxic = pickRandom(['anjing','kontol','bangsat','ngentot','ngewe','memek','tolol'])
 handler.before = m => {
   if (m.isBaileys && m.fromMe) return true
   let chat = global.DATABASE.data.chats[m.chat]
-  let isBadWord = bad.exec(m.text)
-  if (chat.badWord && isBadWord)
-    m.reply(`
- *[ ANTI BADWORD ]*
-            
- _AMPUN BANG JAGO :(_
+  
 
-*_Nanti Kalau Dikick Jangan Protes!!_*
-`.trim())
-  return true    
+  if (chat.antiToxic) m.reply('Kata Toxic Terdeteksi! Anda Akan Dikeluarkan Otomatis Oleh SGDC-BOT')
+  conn.groupRemove([m.chat])
+  return true
 }
 
+
+
+handler.botAdmin = true
+
+handler.fail = null
 
 module.exports = handler
 
