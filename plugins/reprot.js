f969ac7f9e124a21aa829d17363e67d83bbd7d18
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-03-21 11:43:46 +0700

    Add files via upload

--- plugins/reprot.js
+++ plugins/reprot.js
@@ -2,11 +2,18 @@ const { MessageType } = require('@adiwajshing/baileys')
 
 let handler = async(m, { conn, text }) => {
     if (!text) return conn.reply(m.chat, '_Masukkan Text Laporan!_', m)
-    if (text > 300) return conn.reply(m.chat, 'Teks Terlalu Panjang!, Maksimal 300 Karakter', m)
+    if (text > 200) return conn.reply(m.chat, 'Teks Terlalu Panjang!, Maksimal 200 Karakter', m)
+    let name = conn.getName(m.sender)
     var nomor = m.sender
-    const teks1 = `*[[[ • BUG REPORT • ]]]*\n\n*Nomor:* wa.me/${nomor.split("@s.whatsapp.net")[0]}\n*Laporan:* ${text}\n\n*[ • SGDC-BOT •  ]*`
-    conn.sendMessage('6282252655313@s.whatsapp.net', teks1, MessageType.text)
-    conn.reply(m.chat, '*[ SUKSES ]* _Masalah telah di laporkan ke_ *Owner BOT!*', m)
+    const reprot = `
+*╭══════[ REPORT ]══════╮*
+*• Nama:* ${name}
+*• Nomor:* wa.me/${nomor.split("@s.whatsapp.net")[0]}
+*• Laporan:* ${text}
+*╰═════════════════╯*
+`.trim()
+    conn.sendMessage('6282252655313@s.whatsapp.net', reprot, MessageType.text)
+    conn.reply(m.chat, '*[ SUKSES ]* _Masalah telah di laporkan ke_ *Owner Bot!*', m)
 }
 
 handler.command = /^(bug|report|bugreport)$/i
