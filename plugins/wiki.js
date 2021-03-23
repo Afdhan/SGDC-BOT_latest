f8372f55602ccb0e9b8bcb9a7e593fa304a6d56d
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-03-24 00:29:10 +0800

    Add files via upload

---
+++ plugins/wiki.js
@@ -0,0 +1,25 @@
+let axios = require("axios");
+let handler = async(m, { conn, text }) => {
+
+    if (!text) return conn.reply(m.chat, 'Silahkan masukan kata kunci', m)
+
+  await m.reply('Searching...')
+axios.get(`https://st4rz.herokuapp.com/api/wiki?q=${teks}`).then((res) => {
+    let hasil = `*Menurut Wikipedia:*\n\n${res.data.result}
+conn.reply(m.chat, hasil, m)
+	})
+}
+handler.command = /^(wiki|wikisearch)$/i
+handler.owner = true
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = false
+
+handler.admin = false
+handler.botAdmin = false
+
+handler.fail = null
+
+
+module.exports = handler
