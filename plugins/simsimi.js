690d9837edb01a3935804f594dce4daf9e6ce267
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-03-24 01:31:03 +0700

    Update simsimi.js

--- plugins/simsimi.js
+++ plugins/simsimi.js
@@ -1,6 +1,6 @@
 let fetch = require('node-fetch')
 let handler = async (m, { text }) => {
-  let res = await fetch(global.API('xteam', '/simsimi', { kata: text }, 'https://st4rz.herokuapp.com/api/simsimi?kata=${text}'))
+  let res = await fetch('https://st4rz.herokuapp.com/api/simsimi?kata=${text}')
   let json = await res.json()
   if (json.status) m.reply(json.jawaban)
   else throw json
