bbc1d28ee774eaae0008d542621311630a1566c8
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-03-24 00:23:36 +0700

    Update anime.js

--- plugins/anime.js
+++ plugins/anime.js
@@ -1,9 +1,9 @@
 let fetch = require('node-fetch')
 
 let handler = async(m, { conn, args, usedPrefix }) => {
-    if (args.length == 0) return conn.reply(m.chat, `*Untuk menggunakan *${usedPrefix}anime*\nSilahkan ketik: *${usedPrefix}anime* [Query]\nContoh: *${usedPrefix}anime random*\n\n*List Query:\n_> waifu_\n_> husbu_\n_> neko_`, m)
+    if (args.length == 0) return conn.reply(m.chat, `Untuk menggunakan *${usedPrefix}anime*\nSilahkan ketik: *${usedPrefix}anime* [Query]\nContoh: *${usedPrefix}anime random*\n\n*List Query:\n_> waifu_\n_> husbu_\n_> neko_`, m)
     if (args[0] == 'random' || args[0] == 'waifu' || args[0] == 'husbu' || args[0] == 'neko') {
-  await m.reply('*[ WAIT ] _Searching..._')
+  await m.reply('*[ WAIT ]* _Searching..._')
 
         fetch('https://raw.githubusercontent.com/ArugaZ/grabbed-results/main/random/anime/' + args[0] + '.txt')
             .then(res => res.text())
