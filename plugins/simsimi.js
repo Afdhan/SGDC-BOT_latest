cc1581ddbacbbbe95f01d3b4cc8b7b7396b70ae3
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-03-24 01:44:43 +0700

    Update simsimi.js

--- plugins/simsimi.js
+++ plugins/simsimi.js
@@ -3,11 +3,12 @@ let fetch = require('node-fetch')
 let handler = async (m, { text }) => {
 //  let res = await fetch({ kata: text }, `https://st4rz.herokuapp.com/api/simsimi?kata=${text}`)
 //  let json = await res.json()
-axios.get(`https://st4rz.herokuapp.com/api/simsimi?kata=${text}`).then((res) => {
+let res = axios.get(`https://st4rz.herokuapp.com/api/simsimi?kata=${text}`)
   let json = await res.json()
   if (json.status) m.reply(json.jawaban)
   else throw json
-}
+  }
+                                                                       
 handler.help = ['simi', 'simsimi', 'simih'].map(v => v + ' <teks>')
 handler.tags = ['fun']
 handler.command = /^((sim)?simi|simih)$/i
