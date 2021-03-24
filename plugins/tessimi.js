8af753cf9a9ac8ae4d946db27bbdfef104e15d26
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-03-24 16:58:25 +0700

    Update tessimi.js

--- plugins/tessimi.js
+++ plugins/tessimi.js
@@ -1,15 +1,15 @@
 let axios = require("axios");
-let handler = async(m, { conn, text }) => m
-
-let m = /(a|b|c|d|e|f)/i
-handler.before = m => {
+let handler = async (m, { conn, text }) => {
+  
 axios.get(`https://videfikri.com/api/simsimi/?teks=${text}`).then((res) => {  
-  let chat = global.DATABASE.data.chats[m.chat]
+  //let chat = global.DATABASE.data.chats[m.chat]
   let simi = `${res.data.result.jawaban}`
    conn.reply(m.chat, simi, m)
    })
 }
 
-module.exports = handler
+handler.customPrefix = /an|ab|su|mek|tol|ok|log|sat|ck|ik|pus|tod|nge|en|col|ge|ba|he|ka/i
+handler.command = new RegExp
 
+module.exports = handler
 
