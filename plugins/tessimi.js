4576dee71f5188748782309caf1b8b95b4225830
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-03-24 16:52:51 +0700

    Update tessimi.js

--- plugins/tessimi.js
+++ plugins/tessimi.js
@@ -7,6 +7,7 @@ axios.get(`https://videfikri.com/api/simsimi/?teks=${text}`).then((res) => {
   let chat = global.DATABASE.data.chats[m.chat]
   let simi = `${res.data.result.jawaban}`
    conn.reply(m.chat, simi, m)
+   })
 }
 
 module.exports = handler
