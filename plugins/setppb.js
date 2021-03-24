e81d4fef8ea38b9db8c7e1d561dbb19b9b1f16af
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-03-24 23:34:22 +0700

    Update setppb.js

--- plugins/setppb.js
+++ plugins/setppb.js
@@ -1,7 +1,7 @@
 let { Presence } = require('@adiwajshing/baileys')
 let handler  = async (m, { conn, args, usedPrefix, command }) => {
 				    conn.updatePresence(m.chat, Presence.composing) 
-					if (!isQuotedImage) return m.reply(`_Kirim gambar dengan caption *${usedPrefix}setpp* atau tag gambar yang sudah dikirim!_`)
+					//(!isQuotedImage) return m.reply(`_Kirim gambar dengan caption *${usedPrefix}setpp* atau tag gambar yang sudah dikirim!_`)
 					enmedia = JSON.parse(JSON.stringify(mek).replace('quotedM','m')).message.extendedTextMessage.contextInfo
 					media = await conn.downloadAndSaveMediaMessage(enmedia)
 					await conn.updateProfilePicture(botNumber, media)
