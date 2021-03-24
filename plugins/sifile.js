52f334cd5ba7e918ec043bb2b56e9dcc489615de
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-03-24 22:36:26 +0700

    Update sifile.js

--- plugins/sifile.js
+++ plugins/sifile.js
@@ -9,7 +9,7 @@ let url = "https://fzn-gaz.herokuapp.com/api/sfile?search=${text}"
             axios.get(url)
            .then((res) => {
                let sfile = /*JSON.parse(JSON.stringify(*/res.data.result
-               let hasil = `*SFILE SEARCH*\n\n${sfile.data.link}`
+               let hasil = `*SFILE SEARCH*\n\n${sfile.data}`
 conn.reply(m.chat, hasil, m)
 	})
 }
