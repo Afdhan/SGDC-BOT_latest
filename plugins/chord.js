eaa0606ef66fc94223550fae2eafc3c117c73b8f
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-03-24 07:49:14 +0700

    Add files via upload

---
+++ plugins/chord.js
@@ -0,0 +1,25 @@
+let axios = require("axios");
+let handler = async(m, { conn, text }) => {
+
+    if (!text) return conn.reply(m.chat, 'Masukkan Chord Yang Akan Dicari!', m)
+
+  await m.reply('*[ WAIT ]* _Searching..._')
+axios.get(`https://st4rz.herokuapp.com/api/chord?q=${teks}`).then((res) => {
+    let hasil = `${res.data.result}`
+conn.reply(m.chat, hasil, m)
+	})
+}
+handler.command = /^(chord)$/i
+handler.owner = false
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = false
+
+handler.admin = false
+handler.botAdmin = false
+
+handler.fail = null
+
+
+module.exports = handler
