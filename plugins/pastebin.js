215ad0f9fecae02084fd5bd76ba904334010f811
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-03-24 19:54:03 +0700

    Add files via upload

---
+++ plugins/pastebin.js
@@ -0,0 +1,28 @@
+let axios = require("axios");
+let handler = async(m, { conn, text }) => {
+
+    if (!text) return conn.reply(m.chat, 'Silahkan masukan teks', m)
+
+//  await m.reply('*[ WAIT ]* _Seda..._')
+axios.get(`https://lindow-api.herokuapp.com/api/pastebin?query=${text}`).then((res) => {
+    let hasil = `*Teks:* ${text}\n*Link:* ${res.data.message}`
+conn.reply(m.chat, hasil, m)
+	})
+}
+handler.command = /^(pastebin)$/i
+handler.owner = false
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = false
+
+handler.admin = false
+handler.botAdmin = false
+
+handler.fail = null
+
+
+module.exports = handler
+
+
+
