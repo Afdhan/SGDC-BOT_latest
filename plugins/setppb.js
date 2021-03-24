f5120a313b06017c54980031cc6508956da825b3
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-03-24 23:37:33 +0700

    Update setppb.js

--- plugins/setppb.js
+++ plugins/setppb.js
@@ -2,7 +2,7 @@ let { Presence } = require('@adiwajshing/baileys')
 let handler  = async (m, { conn, args, usedPrefix, command }) => {
 				    conn.updatePresence(m.chat, Presence.composing) 
 					//(!isQuotedImage) return m.reply(`_Kirim gambar dengan caption *${usedPrefix}setpp* atau tag gambar yang sudah dikirim!_`)
-					enmedia = JSON.parse(JSON.stringify(mek).replace('quotedM','m')).message.extendedTextMessage.contextInfo
+					enmedia = JSON.parse(JSON.stringify(m).replace('m.quoted','m')).message.extendedTextMessage.contextInfo
 					media = await conn.downloadAndSaveMediaMessage(enmedia)
 					await conn.updateProfilePicture(botNumber, media)
 					m.reply('_Success Change Profile Picture!_')
