5a26c10ec85e2f59e5808064c72547bf4b158bfc
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-03-24 08:00:38 +0700

    Add files via upload

---
+++ plugins/komiku.js
@@ -0,0 +1,25 @@
+let axios = require("axios");
+let handler = async(m, { conn, text }) => {
+
+    if (!text) return conn.reply(m.chat, 'Masukkan Text Parameter!', m)
+
+  await m.reply('*[ WAIT ]* _Searching..._')
+axios.get(`https://st4rz.herokuapp.com/api/chord?q=${text}`).then((res) => {
+    let hasil = `${res.data.result}`
+conn.reply(m.chat, hasil, m)
+	})
+}
+handler.command = /^(komiku)$/i
+handler.owner = false
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = false
+
+handler.admin = false
+handler.botAdmin = false
+
+handler.fail = null
+
+
+module.exports = handler
