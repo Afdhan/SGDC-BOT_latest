44e915fdaaf2314b65d1fc981e0d0cf2db9ba30c
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-03-24 12:58:10 +0700

    Update sifile.js

--- plugins/sifile.js
+++ plugins/sifile.js
@@ -4,7 +4,7 @@ let handler = async(m, { conn, text }) => {
     if (!text) return conn.reply(m.chat, 'Apa Yang Mau Dicari?!', m)
 
   await m.reply('*[ WAIT ]* _Searching..._')
-axios.get(`https://fzn-gaz.herokuapp.com/api/sfile?search=${teks}`).then((res) => {
+axios.get(`https://fzn-gaz.herokuapp.com/api/sfile?search=${text}`).then((res) => {
     let hasil = `*SFILE SEARCH*\n\n${res.data.result}`
 conn.reply(m.chat, hasil, m)
 	})
