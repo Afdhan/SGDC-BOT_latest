92df78b7d10e3dbabc89fbaed90c0cd8074610c8
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-03-24 15:41:21 +0700

    Add files via upload

---
+++ plugins/textbalik.js
@@ -0,0 +1,25 @@
+let axios = require("axios");
+let handler = async(m, { conn, text }) => {
+
+    if (!text) return conn.reply(m.chat, 'Silahkan masukan kata kunci', m)
+
+  //await m.reply('*[ WAIT ]* _Sedang Diproses..._')
+axios.get(`https://videfikri.com/api/hurufterbalik/?query=${text}`).then((res) => {
+    let hasil = `*${res.data.result}`
+conn.reply(m.chat, hasil, m)
+	})
+}
+handler.command = /^(teksbalik)$/i
+handler.owner = false
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = false
+
+handler.admin = false
+handler.botAdmin = false
+
+handler.fail = null
+
+
+module.exports = handler
