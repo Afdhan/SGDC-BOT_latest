fe2221ed963188c7bf156352c3cc317c9dc2cb56
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-03-25 18:56:59 +0700

    Update quotemaker.js

--- plugins/quotemaker.js
+++ plugins/quotemaker.js
@@ -1,14 +1,15 @@
+let axios = require("axios");
 let util = require('util')
 let path = require('path')
 let { spawn } = require('child_process')
 
-let handler  = async (m, { conn, text }) => {
+let handler  = async(m, { conn, text }) => {
 let d = new Date
   let tgl = d.toLocaleDateString('id-Id')
   let hari = d.toLocaleDateString('id-Id', { weekday: 'long' })
  // let [teks, wm] = text.split('|')
-  let wm = '[ • SGDC-BOT • ]'
-  let quot = 'https://api.xteam.xyz/quotemaker?text=' + text + '&wm=' + wm + '&APIKEY=abba3220ce4a347f'
+  let mm = '[ • SGDC-BOT • ]'
+  let quot = 'https://api.xteam.xyz/quotemaker?text=' + text + '&wm=' + mm + '&APIKEY=abba3220ce4a347f'
 conn.sendFile(m.chat, quot, 'SGDC.jpg', '*[ • SGDC-BOT • ]*', m)
 //m.reply('Apikeynya gaada mbah!')
  }
