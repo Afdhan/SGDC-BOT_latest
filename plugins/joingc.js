efd7e7c7b7c46517ffbb722cc6a8d531e842a55c
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-03-25 12:04:11 +0700

    Update joingc.js

--- plugins/joingc.js
+++ plugins/joingc.js
@@ -1,17 +1,16 @@
 const { MessageType } = require('@adiwajshing/baileys')
 
 let handler = async(m, { conn, args, text }) => {
-    if(!text) return conn.reply(m.chat, 'Masukkan link undangan grup!', m)
- } else if(!text = 'https://chat.whatsapp.com/([0-9A-Za-z]{20,24})') {
-     conn.reply(m.chat, 'Link Tidak Valid!', m)
- } else if(text == 'https://chat.whatsapp.com/([0-9A-Za-z]{20,24})') {
+   if(!text) return conn.reply(m.chat, 'Masukkan link undangan grup!', m)
+   if(!text = 'https://chat.whatsapp.com/([0-9A-Za-z]{20,24})') return conn.reply(m.chat, 'Link Tidak Valid!', m)
+  }  if(text == 'https://chat.whatsapp.com/([0-9A-Za-z]{20,24})') {
     var nomor = m.sender
     const teks1 = `*[ UNDANGAN GROUP ]*\n*DARI:* wa.me/${nomor.split("@s.whatsapp.net")[0]}\n*LINK:* ${text}`
     conn.sendMessage('6282252655313@s.whatsapp.net', teks1, MessageType.text)
     conn.reply(m.chat, '_Mengirim Permintaan Ke Owner..!_\n\n*BOT akan join ketika permintaan telah dikonfirmasi oleh Owner!*', m)
- } else {
+ /*} else {
      conn.reply(m.chat, '*MASUKKAN LINK YANG VALID!*', m)
-     }
+     }*/}
  }
 handler.command = /^(join(gc)?)$/i
 handler.owner = false
