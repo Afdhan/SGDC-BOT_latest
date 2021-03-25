393d31bc4d5027f01ca9c3283bc3551ed82aadf0
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-03-25 14:39:23 +0700

    Update _antilink.js

--- plugins/_antilink.js
+++ plugins/_antilink.js
@@ -5,7 +5,12 @@ handler.before = m => {
   if (m.isBaileys && m.fromMe) return true
   let chat = global.DATABASE.data.chats[m.chat]
   let isGroupLink = linkRegex.exec(m.text)
-  if (chat.antiLink && isGroupLink) m.reply('*Link Group Terdeteksi!*\n*_Hapus Atau Kick?!?!_*')
+  if (chat.antiLink && isGroupLink) m.reply('*_Kick?!?!_*')
+  if (global.opts['restrict']) {
+       if (!user.isAdmin) return true
+       this.groupRemove(m.chat, [m.sender])
+    }
+  }
   return true
 }
 
