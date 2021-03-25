967344d8e046d1b2c7c861a6e1f84812dc80618c
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-03-25 11:48:00 +0700

    Update joingc.js

--- plugins/joingc.js
+++ plugins/joingc.js
@@ -2,7 +2,7 @@ const { MessageType } = require('@adiwajshing/baileys')
 
 let handler = async(m, { conn, args, text }) => {
     if(!text) return conn.reply(m.chat, 'Masukkan link undangan grup!', m)
-    if(!text = 'https://chat.whatsapp.com/([0-9A-Za-z]{20,24})') return m.reply('Link Tidak Vaid!')
+    if(!text = 'https://chat.whatsapp.com/([0-9A-Za-z]{20,24})') return conn.reply(m.chat, 'Link Tidak Vaid!', m)
     var nomor = m.sender
     const teks1 = `*[ UNDANGAN GROUP ]*\n*DARI:* wa.me/${nomor.split("@s.whatsapp.net")[0]}\n*LINK:* ${text}`
     if(text == 'https://chat.whatsapp.com/([0-9A-Za-z]{20,24})') return true
