282035e60c9dc5f2ebb278b5aefba902b2b8f7e6
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-03-25 20:02:42 +0700

    Add files via upload

--- plugins/igstalk.js
+++ plugins/igstalk.js
@@ -1,15 +1,29 @@
+let imageToBase64 = require('image-to-base64');
 let axios = require("axios");
 let handler = async(m, { conn, text }) => {
 
-    if (!text) return conn.reply(m.chat, 'Masukkan Username!', m)
-
+ if (!text) return conn.reply(m.chat, 'Masukkan Username Instagram', m)
   await m.reply('*[ WAIT ]* _Searching..._')
-axios.get(`https://mhankbarbar.herokuapp.com/api/stalk?username=${text}`).then((res) => {
-    let hasil = `${res.data.result}`
-conn.reply(m.chat, hasil, m)
-	})
+
+    axios.get(`https://mhankbarbar.herokuapp.com/api/stalk?username=${text}`)
+    .then((res) => {
+      imageToBase64(res.data.Profile_pic)
+        .then(
+          (ress) => {
+            let buf = Buffer.from(ress, 'base64')
+            let str = `
+*Nama:* ${res.data.Name}
+*Username:* ${res.data.Username}
+*Followers:* ${res.data.Jumlah_Followers}
+*Following:* ${res.data.Jumlah_Following}
+*Bio:* ${res.data.Biodata}
+`.trim()
+     conn.sendFile(m.chat, buf, 'SGDC.jpg', str, m)
+        })
+    })
 }
-handler.command = /^(igstalk)$/i
+
+handler.command = /^(tebakgambar)$/i
 handler.owner = false
 handler.mods = false
 handler.premium = false
@@ -20,7 +34,6 @@ handler.admin = false
 handler.botAdmin = false
 
 handler.fail = null
-
+handler.limit = false
 
 module.exports = handler
-
