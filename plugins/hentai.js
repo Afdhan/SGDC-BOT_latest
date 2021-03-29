ae1a1934c8c3905147d659ca95ba2fb267cab64f
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-03-29 23:05:32 +0700

    Update hentai.js

--- plugins/hentai.js
+++ plugins/hentai.js
@@ -1,12 +1,12 @@
 let axios = require("axios");
 let handler = async(m, { conn, text }) => {
 let user = global.DATABASE._data.users[m.sender]
-if (user.prem) {
+if (user.prems) {
   await m.reply('*[ WAIT ]* _Sedang Diproses..._')
 let link = 'https://api.xteam.xyz/randomimage/hentai?APIKEY=abba3220ce4a347f'
 
 conn.sendFile(m.chat, link, 'SGDC-BOT.png', 'Mending lu tobat dah :v\n\n*[ • SGDC-BOT • ]*', m)
-   } else if (!user.prem) m.reply('*FITUR INI KHUSUS UNTUK USER PREMIUM!*')
+   } else if (!user.prems) m.reply('*FITUR INI KHUSUS UNTUK USER PREMIUM!*')
 }
 
 handler.command = /^(hentai)$/i
