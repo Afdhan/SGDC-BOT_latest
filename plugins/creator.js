8a9fb374ecd57e4316370ec9115552873b05de4c
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-03-29 18:43:31 +0000

    7

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,7 +1,8 @@
 let handler = function (m) {
   this.sendContact(m.chat, '6282252655313', '꧁࿇ ᴍ ᴀꜰᴅʜᴀɴ ࿇꧂', m)
-let pp_owner ='src/Owner.jpg'
-conn.sendFile(m.chat, pp_owner, 'My Owner.jpg', '*This is my owner ᴍ ᴀꜰᴅʜᴀɴ*\n*Contact: https://wa.me/6282252655313*', m)
+m.reply('*Jangan Lupa Donasi :v')
+//let pp_owner ='src/Owner.jpg'
+//conn.sendFile(m.chat, pp_owner, 'My Owner.jpg', '*This is my owner ᴍ ᴀꜰᴅʜᴀɴ*\n*Contact: https://wa.me/6282252655313*', m)
 }
 
 handler.command = /^(owner|creator)$/i
