615af0428f07a2e9b07eabb93902bba0ce7a7013
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-03-30 18:16:47 +0700

    Update and rename jawaban_tg.js to _jawaban_tg.js

--- plugins/_jawaban_tg.js
+++ plugins/_jawaban_tg.js
@@ -3,16 +3,16 @@ handler.before = async function (m) {
   let id = m.chat
   if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !/Ketik.*hint/i.test(m.quoted.text)) return
   conn.tebakgambar = conn.tebakgambar ? conn.tebakgambar : {}
-  if (!(id in conn.tebakgambar)) return m.reply('Soal Itu Udah Gaada :v')
+  if (!(id in conn.tebakgambar)) return m.reply('_Lu Telat Njir_')
   if (m.quoted.id == conn.tebakgambar[id][0].id) {
     let json = JSON.parse(JSON.stringify(conn.tebakgambar[id][1]))
     if (m.text.toLowerCase() == json.jawaban.toLowerCase()) {
       global.DATABASE._data.users[m.sender].exp += conn.tebakgambar[id][2]
-      m.reply(`*Jawaban Benar!*\n+${conn.tebakgambar[id][2]} XP`)
+      m.reply(`*_Jawaban Benar!_*\n+${conn.tebakgambar[id][2]} Anda Tidak Mendapatkan Apa-apa :v`)
       clearTimeout(conn.tebakgambar[id][3])
       delete conn.tebakgambar[id]
-    } else if (m.text.toLowerCase().endsWith(json.jawaban.split` `[1])) m.reply(`*Dikit Lagi!*`)
-    else m.reply(`*Jawaban Salah!*`)
+    } else if (m.text.toLowerCase().endsWith(json.jawaban.split` `[1])) m.reply(`*_Dikit Lagi!_*`)
+    else m.reply(`*_Jawaban Salah!_*`)
   }
 }
 handler.exp = 0
