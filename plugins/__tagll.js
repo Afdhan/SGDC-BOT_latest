ba4e1e3bd036d00ea97515dab4e90820d4a56833
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-03-30 23:02:23 +0700

    Add files via upload

---
+++ plugins/__tagll.js
@@ -0,0 +1,23 @@
+let handler = async (m, { conn, text }) => {
+
+let mem = (await conn.groupMetadata(m.chat)).participants.map(u => u.jid)
+//let siapa = member[Math.floor(Math.random() * member.length)]
+let jawab = `@${mem.replace(/@.+/, '')}.join`\n`
+
+    let mentionedJid = [mem]
+    conn.reply(m.chat, jawab, m, { contextInfo: { mentionedJid }})
+}
+
+handler.command = /^tes$/i
+handler.owner = false
+handler.mods = false
+handler.premium = false
+handler.group = true
+handler.private = false
+
+handler.admin = false
+handler.botAdmin = false
+
+handler.fail = null
+
+module.exports = handler
