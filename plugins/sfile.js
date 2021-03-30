7615acb7041218600c6eabb84372e3158a7e41aa
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-03-30 16:11:52 +0700

    Update sfile.js

--- plugins/sfile.js
+++ plugins/sfile.js
@@ -6,16 +6,21 @@ let handler = async(m, { conn, text }) => {
     if (!text) return conn.reply(m.chat, 'Mau cari apa?', m)
 
     await m.reply('*[ WAIT ]* _Sedang diproses..._')
-    get.get(`https://fzn-gaz.herokuapp.com/api/sfile?search=${text}`).length((res) => {
-    let hasil = `${res.data.result}\n==================\n`
-    conn.reply(m.chat, hasil, m)
-  })
+	
+    let res = axios.get(`https://fzn-gaz.herokuapp.com/api/sfile?search=${text}`)
+    let sfil = res.data
+    let hsl = `*「 SFILE SEARCH 」*\n\n*Hasil Pencarian Dari:* ${text}`
+    for (let i = 0; i < sfil.result.length; i++) {
+       hsl += `\n─────────────────\n\n• *Title* : ${sfil.result[i].title}\n• *Link* : ${sfil.result[i].link}\n`
+     //}
+       conn.reply(m.chat, hsl, m)
+    }
 }
 							     
-handler.command = /^(sfilem)$/i
+handler.command = /^(sfile)$/i
 handler.owner = false
 handler.mods = false
-handler.premium = false
+handler.premium = true
 handler.group = false
 handler.private = false
 
