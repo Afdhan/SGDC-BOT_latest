12305ac1bb6943bfb230b7d17caf2caef1699c93
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-03-31 12:51:43 +0700

    Update simsimi.js

--- plugins/simsimi.js
+++ plugins/simsimi.js
@@ -4,7 +4,7 @@ let handler = async (m, { text }) => {
 let chat = global.DATABASE.data.chats[m.chat]
 if (chat.simi) {
  axios.get(`https://api.xteam.xyz/simsimi?kata=${text}&APIKEY=abba3220ce4a347f`).then((res) => {
-  let hh = `${res.data.jawaban}`
+  let hh = `${res.data.result.jawaban}`
 conn.reply(m.chat, hh, m)
   }) 
  } else if (!chat.simi) m.reply('*MODE SIMI BELUM DIAKTIFKAN!*')
