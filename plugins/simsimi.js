395a87f83170f6ca364fa7d4a13b11bfad558195
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-03-31 13:18:26 +0700

    Update simsimi.js

--- plugins/simsimi.js
+++ plugins/simsimi.js
@@ -10,7 +10,7 @@ conn.reply(m.chat, `${res.data.result}`, m)
  } else if (!chat.simi) m.reply('*MODE SIMI BELUM DIAKTIFKAN!*')
 }
                       
-handler.customPrefix = /(a|b|c|d|e|f|g|h|i)/i
+handler.customPrefix = /!/i
 handler.command = new RegExp
 //handler.command = /(?:)/i
 handler.rowner = false
