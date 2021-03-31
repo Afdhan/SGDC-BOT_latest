21e66784640e10d30d1aa467dacc438e86f143bb
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-03-31 12:32:52 +0700

    Update simsimi.js

--- plugins/simsimi.js
+++ plugins/simsimi.js
@@ -1,11 +1,13 @@
 let axios = require('axios')
 let fetch = require('node-fetch')
 let handler = async (m, { text }) => {
-
+let chat = global.DATABASE.data.chats[m.chat]
+if (chat.simi) {
  axios.get(`https://videfikri.com/api/simsimi/?teks=${text}`).then((res) => {
   let hh = `${res.data.result.jawaban}`
 conn.reply(m.chat, hh, m)
-  })
+  }) 
+ } else if (!chat.simi) m.reply('*MODE SIMI BELUM DIAKTIFKAN!*')
 }
                                                                        
 handler.command = /^(si(mi)?(m)?)$/i
