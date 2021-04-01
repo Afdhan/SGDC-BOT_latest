1c34279079a4bc9770282690187f1f32c9d13d08
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-04-01 13:35:13 +0700

    Update oadd.js

--- plugins/oadd.js
+++ plugins/oadd.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn, text }) => {
   let users = text.split`,`.map(v => v.replace(/[^0-9]/g, '') + '@s.whatsapp.net').filter(v => v.length > 20)
   await conn.groupAdd(m.chat, users)
-  m.reply('_Berhasil Nenambahkan Beliau!_')
+  m.reply('_Berhasil Menambahkan Beliau!_')
 }
 
 handler.command = /^(oadd|o\+)$/i
