5fd6511a55b055316290417292d3fd0a41ed0e7e
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-04-01 20:10:17 +0700

    Add files via upload

---
+++ plugins/sfiledl.js
@@ -0,0 +1,37 @@
+let axios = require("axios");
+let handler = async(m, { conn, text }) => {
+
+    if (!text) return conn.reply(m.chat, 'Masukkan Link!', m)
+
+  await m.reply('*[ WAIT ]* _Sedang Diproses..._')
+	axios.get(`https://fzn-gaz.herokuapp.com/api/sfiledl?url=${text}`).then ((res) => {
+	 	let hasil = `
+*HASIL*
+
+*Title:* ${res.data.title}
+*Size:* ${res.data.size}
+*Link:* ${res.data.result}
+
+_Download Sendiri, Jangan Manja :v_
+
+*[ • SGDC-BOT • ]*
+`.trim()
+
+    conn.reply(m.chat, hasil, m)
+	})
+}
+
+handler.command = /^(sfiledl)$/i
+handler.owner = false
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = false
+
+handler.admin = false
+handler.botAdmin = false
+
+handler.fail = null
+
+
+module.exports = handler
