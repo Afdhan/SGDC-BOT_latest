caab1d45994c3990eda3a1967513ecb6ff531a1b
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-04-01 15:34:11 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,19 +1,11 @@
 let handler = function (m) {
-  this.sendContact(m.chat, '6282252655313', '꧁࿇ ᴍ ᴀꜰᴅʜᴀɴ ࿇꧂ || @dhans11__', m)
-m.reply('*Jangan Lupa Donasi :v')
+  this.sendContact(m.chat, '6282252655313', ' ᴍ ᴀꜰᴅʜᴀɴ || @dhans11__', m)
+m.reply('wa.me/6282252655313')
 //let pp_owner ='src/Owner.jpg'
 //conn.sendFile(m.chat, pp_owner, 'My Owner.jpg', '*This is my owner ᴍ ᴀꜰᴅʜᴀɴ*\n*Contact: https://wa.me/6282252655313*', m)
 }
 
 handler.command = /^(owner|creator)$/i
-handler.owner = false
-handler.mods = false
-handler.premium = false
-handler.group = false
-handler.private = false
-
-handler.admin = false
-handler.botAdmin = false
 
 handler.fail = null
 
