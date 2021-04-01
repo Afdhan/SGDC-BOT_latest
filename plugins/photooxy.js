b5ccc2f9e65fed917295cb4cc110a8d9e810d2ec
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-04-02 06:52:04 +0700

    Update photooxy.js

--- plugins/photooxy.js
+++ plugins/photooxy.js
@@ -1,7 +1,7 @@
 let axios = require("axios");
 let handler = async(m, { conn, text }) => {
-let user = global.DATABASE._data.users[m.sender]
-if (user.prems) {
+/*let user = global.DATABASE._data.users[m.sender]
+if (user.prems) {*/
 let [fx, xs] = text.split `|`
     if (!fx) return conn.reply(m.chat, 'Effect tidak boleh kosong!', m)
     if (!xs) return conn.reply(m.chat, 'Silahkan masukan teks', m)
@@ -13,7 +13,7 @@ let [fx, xs] = text.split `|`
 let link = 'https://api.xteam.xyz/photooxy/' + fx + '?text=' + xs + '&APIKEY=abba3220ce4a347f'
 
 conn.sendFile(m.chat, link, 'SGDC-BOT.png', '*[ • SGDC-BOT • ]*', m)
-  } else if (!user.prems) m.reply('*FITUR INI KHUSUS UNTUK USER PREMIUM!*')
+ // } else if (!user.prems) m.reply('*FITUR INI KHUSUS UNTUK USER PREMIUM!*')
 }
 
 handler.command = /^(photooxy)$/i
