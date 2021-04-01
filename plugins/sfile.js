c561a111aa76d8dc23e586fdda8d53820b65de8f
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-04-01 22:01:04 +0700

    Update sfile.js

--- plugins/sfile.js
+++ plugins/sfile.js
@@ -1,33 +1,14 @@
-const { fetchJson } = require('../lib/fetcher')
-let get = require('got')
-let axios = require("axios");
-let handler = async(m, { conn, text }) => {
+let linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})/i
 
-    if (!text) return conn.reply(m.chat, 'Mau cari apa?', m)
-
-    await m.reply('*[ WAIT ]* _Sedang diproses..._')
-	
-    let res = axios.get(`https://fzn-gaz.herokuapp.com/api/sfile?search=${text}`)
-    let sfil = res.data
-    let hsl = `*「 SFILE SEARCH 」*\n\n*Hasil Pencarian Dari:* ${text}`
-    for (let i = 0; i < sfil.result.length; i++) {
-       hsl += `\n─────────────────\n\n• *Title* : ${sfil.result[i].title}\n• *Link* : ${sfil.result[i].link}\n`
-     //}
-       conn.reply(m.chat, hsl, m)
-    }
+let handler = async (m, { conn, text }) => {
+    let [_, code] = text.match(linkRegex) || []
+    if (!code) throw 'Link invalid'
+    let res = await conn.acceptInvite(code)
+    m.reply(`Berhasil join grup ${res.gid}`)
 }
-							     
-handler.command = /^(sfile)$/i
-handler.owner = false
-handler.mods = false
-handler.premium = true
-handler.group = false
-handler.private = false
 
-handler.admin = false
-handler.botAdmin = false
+handler.command = /^(join2)$/i
 
-handler.fail = null
+handler.prems = true
 
-
-module.exports = handler
+module.exports = handler					     
