469309b9c03f1a0c196389a533f6e470fb80b4fb
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-04-01 19:49:15 +0700

    Update joingc.js

--- plugins/joingc.js
+++ plugins/joingc.js
@@ -1,8 +1,7 @@
-let linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})/i
+//let linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})/i
 
 let handler = async (m, { conn, text }) => {
-/*let user = global.DATABASE._data.users[m.sender]
-if (user.prems) {*/
+let linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})/i
     let [_, code] = text.match(linkRegex) || []
     if (!code) throw 'Link invalid'
     let res = await conn.query({
@@ -10,10 +9,9 @@ if (user.prems) {*/
     })
     if (res.status !== 200) throw res
     m.reply(`Berhasil join grup ${res.gid}`)
-//  } else if (!user.prems) m.reply('*FITUR INI KHUSUS UNTUK USER PREMIUM!*')
 }
 
-handler.command = /^join$/i
+handler.command = /^(join)$/i
 handler.owner = false
 handler.mods = false
 handler.premium = true
