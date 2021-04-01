3b1b64aeae7ca78450139bd9128c68cdeecf2309
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-04-01 14:25:05 +0700

    Update semoji.js

--- plugins/semoji.js
+++ plugins/semoji.js
@@ -3,14 +3,13 @@ const { sticker } = require('../lib/sticker')
 const { MessageType } = require('@adiwajshing/baileys')
 
 let handler = async (m, { conn, text }) => {
+ await m.reply('*[ WAIT ]* _Sedang Diproses..._')
 let items = ["whatsapp", "microsoft", "google", "apple", "samsung", "twitter", "facebook", "joypixels", "openmoji", "emojidex", "lg", "htc", "mozilla"]
 let tipe = items[Math.floor(Math.random() * items.length)]
-//let [tipe, emoji] = text.split `|`
  try {
- // if (!tipe) throw '_Silahkan masukan tipe emoji! Misal *!semoji whatsapp*_'
-  if (!text) throw 'Emoji?'
-  let stiker = await sticker(null, global.API('xteam', '/sticker/emojitopng' + tipe, { emo: text }, 'APIKEY'), global.packname, global.author)
-  conn.sendMessage(m.chat, stiker, MessageType.sticker, {
+  if (!text) throw '_Emojinya mana?_'
+  let stic = await sticker(null, global.API('xteam', '/sticker/emojitopng' + tipe, { emo: text }, 'APIKEY'), global.packname, global.author)
+  conn.sendMessage(m.chat, stic, MessageType.sticker, {
     quoted: m
   })
  } catch (e) {
