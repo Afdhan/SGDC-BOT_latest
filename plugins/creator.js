ffc2361d5fd4e65415707861a4092775cddbda8c
Ipan420 <81342478+Ipan420@users.noreply.github.com>
2021-04-04 12:31:03 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,6 +1,6 @@
 let handler = function (m) {
-  this.sendContact(m.chat, '6282252655313', ' ᴍ ᴀꜰᴅʜᴀɴ || @dhans11__', m)
-m.reply('wa.me/6282252655313')
+  this.sendContact(m.chat, '6282286818715', ' IPAN-XYZ || @dhans11__', m)
+m.reply('wa.me/6282286818715')
 //let pp_owner ='src/Owner.jpg'
 //conn.sendFile(m.chat, pp_owner, 'My Owner.jpg', '*This is my owner ᴍ ᴀꜰᴅʜᴀɴ*\n*Contact: https://wa.me/6282252655313*', m)
 }
