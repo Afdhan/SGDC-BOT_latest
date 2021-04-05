5d07f96e3349a77b20317c3488bf863bbc798432
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-04-05 18:33:55 +0800

    Ngontol

--- install.sh
+++ install.sh
@@ -4,3 +4,7 @@ pkg install libwebp -y
 pkg install imagemagick -y
 npm install
 npm audit fix
+
+echo "*** WELCOME TO SCRIPT SGDC-BOT ***"
+echo "SCRIPT GRATISAN UNTUK KAUM GRATISAN"
+echo "KETIK (npm start) UNTUK MEMULAI"
