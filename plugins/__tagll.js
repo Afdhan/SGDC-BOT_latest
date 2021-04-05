6a2a23c1e952e9f2183b9d5045749152bcf92702
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-04-05 11:37:58 +0700

    Update __tagll.js

--- plugins/__tagll.js
+++ plugins/__tagll.js
@@ -1,27 +1,32 @@
 let { MessageType } = require('@adiwajshing/baileys')
-let handler = async (m, { conn, text }) => {
 
-let user = (await conn.groupMetadata(m.chat)).participants.map(u => u.jid)
-//let tt = member.length
-let b =  user.split('@s.whatsapp.net')[0]
-/*let jawab = `
-*Siapa ${text}? Jawabannya si* @${member[tt].m.replace(/@.+/, '')}\n
-`.trim()*/
-conn.sendMessage(m.chat, `@${b}`, MessageType.extendedText, { contextInfo: { mentionedJid: user } })
-    //let mentionedJid = [tt]
-    //ggconn.reply(m.chat, b, m, { contextInfo: { mentionedJid }})
+let handler = async (m, { conn, text }) => {
+  let users = (await conn.groupMetadata(m.chat)).participants.map(u => u.jid)
+  let mem = `*「 TAG ALL MEMBERS 」*\n`
+  for (let i = 0; i < users.length; i++) {
+                    mem += `@${users[i].split("@")[0]}\n`
+                }
+                    mem += '\n*「  SGDC-BOT 」*'
+  conn.reply(m.chat, mem, m, {
+           contextInfo: { 
+                 mentionedJid: users 
+          } 
+     })
 }
 
-handler.command = /^tes$/i
+handler.command = /^(tagall|mentionall)$/i
 handler.owner = false
 handler.mods = false
 handler.premium = false
 handler.group = true
 handler.private = false
 
-handler.admin = false
+handler.admin = true
 handler.botAdmin = false
 
 handler.fail = null
 
 module.exports = handler
+
+
+// BY M AFDHAN YA OM
