384eeb53e42a0ad6ce8bd365a0a7509b18c7490a
DAMARJATI66 <82052326+DAMARJATI66@users.noreply.github.com>
2021-04-07 00:27:51 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 let sgdc = require('./sgdc.json')
 
 global.owner = [(sgdc.owner)]
-global.mods = ['6282252655313','6283159600193']
+global.mods = ['6285707101183','6285707101183']
 global.packname = (sgdc.packname)
 global.author = (sgdc.author)
-global.prems = ['6282252655313','6283122891105']
+global.prems = ['6285707101183','6285707101183']
 global.APIs = { 
   nrtm: 'https://nurutomo.herokuapp.com',
   xteam: 'https://api.xteam.xyz'
