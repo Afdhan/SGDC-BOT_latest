bb602c809dde35a04363b8148ae698b23e154c66
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-04-06 15:49:37 +0800

    Add Sfile.Mobi Search

--- plugins/Sfilemobi.js
+++ plugins/Sfilemobi.js
@@ -2,17 +2,18 @@ let axios = require("axios");
 let handler = async(m, { conn, text }) => {
             if (!text) return conn.reply(m.chat, 'Apa Yang Mau Dicari?', m)
             
-            await m.reply('Wait.....')
+            await m.reply('*[ WAIT ]* _Searching..._')
             axios.get(`https://fzn-gaz.herokuapp.com/api/sfile?search=${text}`).then ((res) => {
                 let sfiles = res.data
                 let sfile = `*「 SFILE SEARCH 」*\n\n*Hasil Pencarian:* ${text}\n`
                 for (let i = 0; i < sfiles.result.length; i++) {
-                    sfile += `\n─────────────────\n\n• *Title:* ${sfiles.result[i].title}\n• *Size:* ${sfiles.result[i].size}\n• *Link:* ${sfiles.result[i].link}\n`
+                    sfile += `─────────────────\n• *Title:* ${sfiles.result[i].title}\n• *Size:* ${sfiles.result[i].size}\n• *Link:* ${sfiles.result[i].link}\n`
                 }
+		    sfile += '*[ • SGDC-BOT • ]*'
                  conn.reply(m.chat, sfile, m)
 	})
 }
-handler.command = /^(sfile)$/i
+handler.command = /^(sfile(search)?)$/i
 handler.owner = false
 handler.mods = false
 handler.premium = false
