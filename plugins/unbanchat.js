d396292470ef96439e2db14025b2b93f39421b55
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-05-26 21:36:59 +0700

    Delete plugins directory

--- plugins/unbanchat.js
+++
@@ -1,9 +0,0 @@
-let handler = async (m, { conn }) => {
-  global.DATABASE._data.chats[m.chat].isBanned = false
-  m.reply('*Berhasil Diunbanned!*')
-}
-
-handler.command = /^(unbanchat|unban)$/i
-handler.owner = true
-
-module.exports = handler
