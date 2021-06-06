b9879c07a94ada6a4636d4f3508f09c9c9a36f7e
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-06-06 20:08:01 +0700

    Full up

--- plugins/sshindo.js
+++ plugins/sshindo.js
@@ -4,11 +4,11 @@ let fs = require('fs')
 let { spawn } = require('child_process')
 let handler = async (m, { conn, args }) => {
 await m.reply(global.wait)
-let thumb = fs.readFileSync("https://telegra.ph/file/2e6035d48c86dc776f17f.jpg")
+let thumb = fs.readFileSync("./src/SGDC-BOT.jpg")
 try {
   let sshindo = 'src/SSH.png'
   await m.reply(global.wait)
-  conn.sendFile(m.chat, sshindo, 'SSH_SGDC.png','Thank\'s to Damar', m, { thumbnail: thumb })
+  conn.sendFile(m.chat, sshindo, '','Thank\'s to Damar', m, { thumbnail: thumb })
 }catch (e){
 	m.reply("```ERROR```")
 	console.log (e)
